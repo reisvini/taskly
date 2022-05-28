4d7db9848b4de5dc01a2dc0654ce311a49716c2e
Vinicius Reis <vinicius.moreira2003@gmail.com>
2022-05-28 14:11:08 -0300

    feat: finished app

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:taskly/models/task.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,6 +12,9 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   late double _deviceHeight;
   late double _deviceWidth;
+
+  String? _newTaskContent;
+  Box? _box;
 
   @override
   Widget build(BuildContext context) {
@@ -23,24 +28,86 @@ class _HomePageState extends State<HomePage> {
             'Taskly!',
             style: TextStyle(fontSize: 25),
           )),
-      body: _taskList(),
+      body: _taskView(),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: handleTaskPopup,
         child: const Icon(Icons.add),
       ),
     );
   }
 
+  Widget _taskView() {
+    return FutureBuilder(
+        future: Hive.openBox('tasks'),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasData) {
+            _box = snapshot.data;
+            return _taskList();
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        });
+  }
+
   Widget _taskList() {
-    return ListView(
-      children: [
-        ListTile(
-          title: const Text("Do homework!",
-              style: TextStyle(decoration: TextDecoration.lineThrough)),
-          subtitle: Text(DateTime.now.toString()),
-          trailing: const Icon(Icons.check_box_outlined, color: Colors.red),
-        )
-      ],
+    List tasks = _box!.values.toList();
+
+    return ListView.builder(
+      itemCount: tasks.length,
+      itemBuilder: (BuildContext context, int index) {
+        var task = Task.fromMap(tasks[index]);
+        return ListTile(
+          title: Text(task.content,
+              style: TextStyle(
+                  decoration:
+                      task.done == false ? null : TextDecoration.lineThrough)),
+          subtitle: Text(task.timestamp.toString()),
+          trailing: Icon(
+              task.done
+                  ? Icons.check_box_outlined
+                  : Icons.check_box_outline_blank_outlined,
+              color: Colors.red),
+          onTap: () {
+            task.done = !task.done;
+            _box!.putAt(index, task.toMap());
+            setState(() {});
+          },
+          onLongPress: () {
+            _box!.deleteAt(index);
+            setState(() {});
+          },
+        );
+      },
     );
+  }
+
+  void handleTaskPopup() {
+    showDialog(
+        context: context,
+        builder: (BuildContext _context) {
+          return AlertDialog(
+            title: const Text('Add New Task!'),
+            content: TextField(
+              onSubmitted: (value) {
+                if (_newTaskContent != null) {
+                  Task _newTask = Task(
+                      content: _newTaskContent!,
+                      timestamp: DateTime.now(),
+                      done: false);
+                  _box?.add(_newTask.toMap());
+                  setState(() {
+                    _newTaskContent = null;
+                    Navigator.pop(context);
+                  });
+                }
+              },
+              onChanged: (value) {
+                setState(() {
+                  _newTaskContent = value;
+                });
+              },
+            ),
+          );
+        });
   }
 }
